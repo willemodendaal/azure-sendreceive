eb46c88127f89141bf071e125cf93600ff772d0e
willem <willem@newmedialabs.co.za>
2015-06-08 16:37:13 +0200

    First commit.

---
+++ Receiver/Program.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Azure;
+using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("RECEIVER (server)");
+
+            ListenForMessages();
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey(true);
+        }
+
+
+        private static void ListenForMessages()
+        {
+            Console.WriteLine("Listening for messages on queue...");
+            string queueName = "queue1";
+
+            string connectionString = CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ReceiverConnectionString");
+            QueueClient Client =
+              QueueClient.CreateFromConnectionString(connectionString, queueName);
+
+            // Configure the callback options
+            OnMessageOptions options = new OnMessageOptions();
+            options.AutoComplete = false;
+            options.AutoRenewTimeout = TimeSpan.FromMinutes(1);
+
+            // Callback to handle received messages
+            Client.OnMessage((message) =>
+            {
+                try
+                {
+                    Console.WriteLine("Got a message. Responding to client...");
+                    SendResponseToTopic(message);
+
+                    // Remove message from queue
+                    message.Complete();
+                }
+                catch (Exception exc)
+                {
+                    // Indicates a problem, unlock message in queue
+                    message.Abandon();
+                    Console.WriteLine("* Error: " + exc.Message);
+                }
+            }, options);
+        }
+
+        private static void SendResponseToTopic(BrokeredMessage message)
+        {
+            
+            string sessionId = message.ReplyToSessionId;
+            string body = message.GetBody<string>();
+            Console.WriteLine("Body: " + body + ", ReplyToSessionID: " + sessionId);
+
+            string connectionString = CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ResponseConnectionString");
+
+            TopicClient Client =
+                TopicClient.CreateFromConnectionString(connectionString, "response1");
+
+            Client.Send(new BrokeredMessage("response_" + body)
+                {
+                   SessionId = sessionId //Ensure we are only responding to somebody waiting for this session message.
+                });
+
+            Console.WriteLine("Sent response with sessionId: " + sessionId);
+        }
+    }
+}
---
+++ Sender/Program.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Azure;
+using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SENDER (client)");
+            Console.WriteLine("Press any key to send a message to the server...");
+            Console.ReadKey(true);
+
+            Random r = new Random();
+            int messageId = r.Next(1000);
+
+            SendMessage(messageId);
+            ListenForResponse(messageId);
+
+            Console.WriteLine("Done. Press any key to exit.");
+            Console.ReadKey(true);
+
+        }
+
+      
+        private static void SendMessage(int messageId)
+        {
+            Console.WriteLine("Sending to the server...");
+            string connectionString = CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.QueueConnectionString");
+            string queueName = "queue1";
+
+            QueueClient queueClient = QueueClient.CreateFromConnectionString(connectionString, queueName);
+
+            string msg = "msg" + messageId;
+            var brokered = new BrokeredMessage(msg)
+                {
+                    ReplyToSessionId = Convert.ToString(messageId) //Send and specify this message has a sessionID response.
+                };
+            queueClient.Send(brokered);
+
+            Console.WriteLine("Sent " + msg);
+        }
+
+        private static void ListenForResponse(int messageId)
+        {
+
+            string subName = "SenderSub";
+            Console.WriteLine("Listening for response from server... ");
+
+            string connectionString = CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.TopicConnectionString");
+
+            SubscriptionClient Client =
+                SubscriptionClient.CreateFromConnectionString
+                        (connectionString, "response1", subName);
+            var session = Client.AcceptMessageSession(Convert.ToString(messageId), new TimeSpan(0,0,20)); //Wait 20 seconds for messages with the specified session ID.
+
+            var message = session.Receive();
+           
+            try
+            {
+                // Process message from subscription
+                Console.WriteLine("\n**Message Received**");
+                Console.WriteLine("Body: " + message.GetBody<string>());
+                Console.WriteLine("SessionID: " + message.SessionId + " (must only be " + messageId + ")");
+                    
+
+                // Remove message from subscription
+                message.Complete();
+            }
+            catch (Exception exc)
+            {
+                // Indicates a problem, unlock message in queue
+                message.Abandon();
+                Console.WriteLine("* Error: " + exc.Message);
+            }
+        }
+    }
+}
